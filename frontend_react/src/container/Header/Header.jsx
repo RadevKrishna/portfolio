218e004a5c31e93898bb4c12c0bbcc3ee11ac6c2
RADEV KRISHNA MR <radevkrishna2003@gmail.com>
2024-11-10 18:31:24 +0530

    header compteted

---
+++ frontend_react/src/container/Header/Header.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Header.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+const scaleVarients = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const Header = () => {
+  return (
+    <div className="app__header app__flex" id="home">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 1 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello I'm</p>
+              <h1 className="head-text">Radev Krishna</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Developer</p>
+            <p className="p-text">IT Engineer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 1, delayChildren: 1 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="Profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="overlay_circle"
+          src={images.circle}
+          alt="profile_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVarients}
+        whileInView={scaleVarients.whileInView}
+        className="app__header-circles"
+      >
+        {[images.vtk, images.python2, images.react].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Header;
